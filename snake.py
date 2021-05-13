0bf1e9be4e639ad2fbea813ccb6f10e83472482b
PortalHubYT <portalhub.skyblock@gmail.com>
2021-05-13 19:41:09 +0000

    changes

---
+++ snake.py
@@ -0,0 +1,125 @@
+from mcpi.minecraft import Minecraft
+
+import sys
+import time
+import random
+
+start_time = time.time()
+mc = Minecraft.create()
+
+mc.postToChat("start")
+
+mc_height = 70
+WIDTH,  HEIGHT = 50, 50
+grid = [[0 for j in range(HEIGHT)] for i in range(WIDTH)]
+
+# def init_snake
+snake = [(10, 1), (2, 1), (3, 1), (3, 1), (3, 1), (3, 1), (3, 1), (3, 1), (3, 1)]
+food = (10, 10)
+direction = 0
+
+def new_food():
+    global food
+    food = (random.randint(0, WIDTH), random.randint(0, HEIGHT))
+
+# game_speed 
+# game_tick
+
+
+
+def remove_tail():
+    rem = snake.pop()
+    mc.setBlock(rem[0], mc_height, rem[1], 35, 0)
+
+def draw_food():    
+    mc.setBlock(food[0], mc_height, food[1], 35, 15)
+
+
+
+def get_direction():
+    vec = mc.player.getTilePos()
+    pos = (vec.x, vec.y, vec.z)
+    global direction
+    if pos[0] < WIDTH / 4 and direction != 2:
+        direction = 3
+    elif pos[0] > 3 * (WIDTH/4) and direction != 3:
+        direction = 2
+    elif pos[2] < HEIGHT / 4 and direction != 0:
+        direction = 1
+    elif pos[2] > 3 * (HEIGHT/4) and direction != 1:
+        direction = 0
+    print(direction)
+    # mc.postToChat(f"{pos}, {direction}")
+    
+
+def move_snake():
+    head = snake[0]
+    print(direction)
+    if direction == 0:
+        new = (head[0], head[1] + 1)
+    elif direction == 1:
+        new = (head[0], head[1] - 1)
+    elif direction == 2:
+        new = (head[0] + 1, head[1])
+    elif direction == 3:
+        new = (head[0] - 1, head[1])
+    
+    
+    if new[0] >= WIDTH:
+        new = (0, new[1])
+    elif new[0] < 0:
+        new = (WIDTH - 1, new[1])
+    
+    if new[1] >= HEIGHT:
+        new = (new[0], 0)
+    elif new[1] < 0:
+        new = (new[0], HEIGHT - 1)
+    
+    if new == food:
+        new_food()
+    else:
+        remove_tail()
+        
+    snake.insert(0, new)
+    mc.setBlock(new[0], mc_height, new[1], 35, 15)
+    
+
+def draw_snake():
+    for x, y in snake:
+        print(x, y)
+        
+        mc.setBlock(x, mc_height, y, 35, 15)
+
+
+def minecraft_draw_grid():
+    for y in range(HEIGHT):
+        for x in range(WIDTH):
+            mc.setBlock(x, mc_height, y, 35, grid[y][x])
+    
+
+    
+def set_pixel(x, y, n):
+    grid[x][y] = n
+
+
+def render():
+    time.sleep(0.03)
+
+    
+    
+    get_direction()
+    move_snake()
+    draw_food()
+
+
+for y in range(HEIGHT):
+    for x in range(WIDTH):
+        set_pixel(x, y, 0)
+            
+
+minecraft_draw_grid()
+draw_snake()
+while True:
+    render()
+
+        
--- test_alex.py
+++ test_alex.py
@@ -1,40 +1,14 @@
-from collections import namedtuple
+import time
+var = time.time()
+from mcapy_lib import *
+print(time.time() - var)
 
-# mc.post("setblock 0 4 0 bedrock")
-# set_block(Coordinates, Block)
-class Coordinates:
-    def __init__(self, x=None, y=None, z=None):
-        self.x = x
-        self.y = y
-        self.z = z
-    
-    def to_str(self):
-        return (f'{self.x} {self.y} {self.z}')
-        
-class Item:
-    pass
+setblock(Coordinates(0, 4, 0), Block("bedrock"))
+setblock(Coordinates(2, 4, 0), Block("bedrock"))
+setblock(Coordinates(4, 4, 0), Block("bedrock"))
 
-class Block:
-    def __init__(self, namespace=None, id=None):
-        self.namespace = namespace
-        self.id = id
+time.sleep(1)
 
-    def to_str(self):
-        return (f'{self.namespace}:{self.id}')
-        
-
-class Command:
-    def __init__(self, name='', args=[''], usage=""):
-        self.name = name
-        self.ags = args
-        self.usage = usage
-
-    def str_renderer:
-        func
-
-# set_block = Command('setblock', [Coordinates, Block, ['destroy', 'keep', 'replace']], "setblock <pos> <block> [destroy|keep|replace]")
-
-def set_block(coords, block):
-    mc.post(f'setblock {coords} {block}')
-
-
+setblock(Coordinates(4, 4, 0), Block("air"))
+setblock(Coordinates(2, 4, 0), Block("air"))
+setblock(Coordinates(0, 4, 0), Block("air"))
