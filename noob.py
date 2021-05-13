2258656bf474953be2e033def9b0abd42ddba80b
MrCyclopede <MrCyclopede@github.com>
2021-05-13 03:26:09 +0000

    add gitignore

--- noob.py
+++ noob.py
@@ -1,13 +1,16 @@
+import time
+import logging
 
 from container_driver import ServerInstance
+logging.basicConfig(level=logging.INFO)
 
 
 mc = ServerInstance(111, 'noob')
 
-import time
-for _ in range(250):
-    time.sleep(1)
-    mc.post(f"say Noob says {_}")
 
-print("stoping instance")
+for _ in range(150):
+    time.sleep(1)
+    mc.post(f'say noob say {_}')
+
+logging.info("Stopping instance after 150 cycles")
 mc.stop()
