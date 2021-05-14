3e853352f211e728f814fa205729d68f8c0918e8
PortalHubYT <portalhub.skyblock@gmail.com>
2021-05-14 01:02:18 +0000

    changes

--- test_alex.py
+++ test_alex.py
@@ -3,12 +3,12 @@ var = time.time()
 from mcapy_lib import *
 print(time.time() - var)
 
-setblock(Coordinates(0, 4, 0), Block("bedrock"))
-setblock(Coordinates(2, 4, 0), Block("bedrock"))
-setblock(Coordinates(4, 4, 0), Block("bedrock"))
 
-time.sleep(1)
 
-setblock(Coordinates(4, 4, 0), Block("air"))
-setblock(Coordinates(2, 4, 0), Block("air"))
-setblock(Coordinates(0, 4, 0), Block("air"))
+
+pos1 = Coordinates(0, 4, 0)
+pos2 = Coordinates(10, 4, 10)
+to_place =  Block("bedrock")
+to_replace = Block("air")
+
+fill(pos1, pos2, to_place)
