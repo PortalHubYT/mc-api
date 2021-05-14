eac4b836c543fd78651eb5f5e8ed4efe3c4c0597
PortalHubYT <portalhub.skyblock@gmail.com>
2021-05-14 14:00:11 +0000

    changes in structure and in setup

--- mc_api/setup/test/test_runner.py
+++ mc_api/setup/test/test_runner.py
@@ -1,16 +1,16 @@
 import unittest
-import subprocess
-from os import path
+
+try:
+    from mc_api.setup.runner import Runner
+except ModuleNotFoundError:
+    from runner import Runner
 
 class TestRunner(unittest.TestCase):
 
-    def test_runner(self):
-        subprocess.run(['sh', 'run.sh'], stderr=subprocess.DEVNULL, stdout=subprocess.DEVNULL)
-        self.assertTrue(True)
-
-    def test_output(self):
-        self.assertEqual(path.exists('generated'), True)
-        
+    def test_run(self):
+        tasks = Runner()
+        tasks.run()
+ 
 if __name__ == '__main__':
     unittest.main()
     
