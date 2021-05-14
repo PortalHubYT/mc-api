453654ee01e5be219e6741cbdf7064c8b8883931
MrCyclopede <MrCyclopede@github.com>
2021-05-14 20:41:35 +0000

    improved test_command and added testcase

--- mc_api/components/Command.py
+++ mc_api/components/Command.py
@@ -6,9 +6,12 @@ class Command:
         self.args = args
         
     def to_str(self):
-        buff = self.command + ' '
-        for a in self.args[:-1]:
-            buff += flatten(a)
+        buff = self.command
+        if len(self.args) == 0:
+            return buff
+        for a in self.args:
             buff += ' '
-        buff += flatten(self.args[-1])
+            buff += flatten(a)
+            
+        
         return buff
--- mc_api/components/test/test_Command.py
+++ mc_api/components/test/test_Command.py
@@ -3,6 +3,7 @@ import unittest
 from mc_api.components.Command import Command
 from mc_api.components.Block import Block
 from mc_api.components.Coordinates import Coordinates
+from mc_api.components.BlockState import BlockState
 
 class TestCommand(unittest.TestCase):
 
@@ -12,10 +13,21 @@ class TestCommand(unittest.TestCase):
         self.assertEqual(diff, test)
     
     def test_command_with_args(self):
-        diff = Command("setblock", [Coordinates(0, 0, 0), Block("bedrock")]).to_str()
+        diff = Command("setblock", Coordinates(0, 0, 0), Block("bedrock")).to_str()
         test = "setblock 0 0 0 minecraft:bedrock"
         self.assertEqual(diff, test)
-        
+    
+    def test_fill_wblockstate(self):
+        pos1 = Coordinates(0, 4, 0)
+        pos2 = Coordinates(10, 4, 10)
+        facing = BlockState("facing", "north")
+        half = BlockState("half", "top")
+        block = Block("oak_stairs", blockstate = [facing, half])
+
+        command = Command('fill', pos1, pos2, block)
+        expected = "fill 0 4 0 10 4 10 minecraft:oak_stairs[facing=north,half=top]"
+        self.assertEqual(command.to_str(), expected)
+
 if __name__ == '__main__':
     unittest.main()
     
