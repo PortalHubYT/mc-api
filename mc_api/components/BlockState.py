9ae175ba870baa46d53eaf22c21e23a9483d7323
PortalHubYT <portalhub.skyblock@gmail.com>
2021-05-14 14:07:06 +0000

    additional changes

--- mc_api/components/BlockState.py
+++ mc_api/components/BlockState.py
@@ -1,6 +1,7 @@
 class BlockState:
-    def __init__(self, type):
-        self.type = type
+    def __init__(self):
+        self.key = key
+        self.value = value
 
     def to_str(self):
-        return(f'{self.type}')
+        return(f'[{self.type}={self.value}]')
