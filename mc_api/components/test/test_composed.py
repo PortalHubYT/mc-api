dfe82a4078ea2aec268707f2cf7a180f92563fbe
MrCyclopede <MrCyclopede@github.com>
2021-05-15 22:50:29 +0000

    edited tests so they don't use other components

--- mc_api/components/test/test_composed.py
+++ mc_api/components/test/test_composed.py
@@ -11,3 +11,14 @@ class TestBlockHandler(unittest.TestCase):
         diff = Command('setblock', Coordinates(0, 0, 0), Block('bedrock'), BlockHandler('destroy')).to_str()
         test = "setblock 0 0 0 minecraft:bedrock destroy"
         self.assertEqual(diff, test)
+
+    def test_fill_wblockstate(self):
+        pos1 = Coordinates(0, 4, 0)
+        pos2 = Coordinates(10, 4, 10)
+        facing = BlockState("facing", "north")
+        half = BlockState("half", "top")
+        block = Block("oak_stairs", blockstate = [facing, half])
+
+        command = Command('fill', pos1, pos2, block)
+        expected = "fill 0 4 0 10 4 10 minecraft:oak_stairs[facing=north,half=top]"
+        self.assertEqual(command.to_str(), expected)
