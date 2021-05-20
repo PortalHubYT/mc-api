b9ecb5c9fdb10e98084742a8d285ff9c0e7d213d
PortalHubYT <portalhub.skyblock@gmail.com>
2021-05-20 14:06:22 +0000

    refactored the BlockState component to be production ready, including the related test suite

--- mc_api/components/BlockState.py
+++ mc_api/components/BlockState.py
@@ -1,10 +1,50 @@
 class BlockState:
-    def __init__(self, property=None, value=None):
-        self.property = property
-        self.value = value
+
+    def __init__(self, properties: dict = None):
+        
+        self.buff = ''
+
+        if type(properties) is dict:
+
+            for key in properties:
+            
+                properties[key] = self.flatten(properties[key])
+
+                self.buff += f'{key}={properties[key]},'
+
+    def flatten(self, arg):
+        if type(arg) is int:
+            return(str(arg))
+
+        elif type(arg) is bool:
+            if arg is True:
+                return('true')
+            else:
+                return('false')
+        
+        elif type(arg) is str:
+            return(arg)
+        
+        else:
+            raise UnexpectedBlockStatePropertyValueType(f'The type of: [{arg}] is [{type(arg)}] and is not valid for blockstate. The value must either be an int, a boolean or a string')
 
     def __repr__(self):
-        if self.property and self.value:
-            return(f'{self.property}={self.value}')
-        else:
-            return(f'')
+        
+        for key in dir(self):
+            if key.startswith('__'):
+                continue
+            elif key in ['buff', 'flatten']:
+                continue
+        
+            value = getattr(self, key)
+            value = self.flatten(value)
+
+            self.buff += f'{key}={value},'
+
+        if len(self.buff) > 1 and self.buff[-1] == ',':
+            self.buff = self.buff[:-1]
+
+        return(f'[{self.buff}]')
+
+class UnexpectedBlockStatePropertyValueType(Exception):
+    pass
---
+++ mc_api/components/test/test_BlockState.py
@@ -0,0 +1,112 @@
+import unittest
+
+from mc_api.components.BlockState import BlockState
+from mc_api.components.Block import Block
+
+class TestBlock(unittest.TestCase):
+
+    def test_blockstate_without_args(self):
+        bs = BlockState()
+
+        diff = repr(bs)
+        test = "[]"
+        self.assertEqual(diff, test)
+    
+    def test_blockstate_without_attrs(self):
+        b = Block('dirt')
+
+        diff = repr(b.blockstate)
+        test = "[]"
+        self.assertEqual(diff, test)
+    
+    def test_blockstate_without_args_passed_to_Block(self):
+        bs = BlockState()
+        b = Block('dirt', blockstate=bs)
+
+        diff = repr(b.blockstate)
+        test = '[]'
+        self.assertEqual(diff, test)
+
+    def test_blockstate_without_args_attributed_to_Block(self):
+        bs = BlockState()
+        b = Block('dirt')
+
+        b.blockstate = bs
+
+        diff = repr(b.blockstate)
+        test = '[]'
+        self.assertEqual(diff, test)
+
+    def test_blockstate_with_arg(self):
+        bs = BlockState({"snowy": True})
+
+        diff = repr(bs)
+        test = '[snowy=true]'
+        self.assertEqual(diff, test)
+    
+    def test_blockstate_with_args(self):
+        bs = BlockState({"snowy": True, "facing": "north"})
+
+        diff = repr(bs)
+        test = '[snowy=true,facing=north]'
+        self.assertEqual(diff, test)
+
+    def test_blockstate_via_attr(self):
+        bs = BlockState()
+        bs.snowy = True
+
+        diff = repr(bs)
+        test = '[snowy=true]'
+        self.assertEqual(diff, test)
+
+    def test_blockstate_via_attrs(self):
+        bs = BlockState()
+        bs.snowy = True
+        bs.facing = "north"
+
+        diff = repr(bs)
+        test = '[facing=north,snowy=true]'
+        self.assertEqual(diff, test)
+
+    def test_blockstate_with_arg_passed_to_Block(self):
+        bs = BlockState({"snowy": True})
+        b = Block('dirt', blockstate=bs)
+
+        diff = repr(b.blockstate)
+        test = '[snowy=true]'
+        self.assertEqual(diff, test)
+
+    def test_blockstate_with_args_passed_to_Block(self):
+        bs = BlockState({"snowy": True, "facing": "north"})
+        b = Block('dirt', blockstate=bs)
+
+        diff = repr(b.blockstate)
+        test = '[snowy=true,facing=north]'
+        self.assertEqual(diff, test)
+
+    def test_blockstate_via_attr_through_Block(self):
+        b = Block('dirt')
+        b.blockstate.snowy = True
+
+        diff = repr(b.blockstate)
+        test = '[snowy=true]'
+        self.assertEqual(diff, test)
+
+    def test_blockstate_via_attrs_through_Block(self):
+        b = Block('dirt')
+        b.blockstate.snowy = True
+        b.blockstate.facing = 'north'
+
+        diff = repr(b.blockstate)
+        test = '[facing=north,snowy=true]'
+        self.assertEqual(diff, test)
+
+    def test_blockstate_mixed_attrs_and_args_through_Block(self):
+        bs = BlockState({"snowy": True, "dancing": "well"})
+        b = Block('dirt')
+        b.blockstate = bs
+        b.blockstate.facing = 'north'
+
+        diff = repr(b.blockstate)
+        test = '[snowy=true,dancing=well,facing=north]'
+        self.assertEqual(diff, test)
