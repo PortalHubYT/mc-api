48a44010f44dbb9b664a13650999211621b57850
PortalHubYT <portalhub.skyblock@gmail.com>
2021-05-25 13:01:04 +0000

    refactored BlockHandler to be a release candidate and added the corresponding test suite

--- mc_api/components/BlockHandler.py
+++ mc_api/components/BlockHandler.py
@@ -1,47 +1,40 @@
-from mc_api.components import Block
-
 options = ['destroy', 'hollow', 'keep', 'outline', 'replace']
 
 class BlockHandler:
-    def __init__(self, mode: str, option: Block = None):
-        self.mode = mode
+    """
+    DESTROY: Replaces all blocks (including air) in the fill region with the specified block, 
+    dropping the existing blocks (including those that are unchanged) and block contents 
+    as entities as if they had been mined with an unenchanted diamond shovel or pickaxe. 
+    (Blocks that can only be mined with shears, such as vines, will not drop; neither will liquids.)
+
+    HOLLOW: Replaces only blocks on the outer edge of the fill region with the specified block. 
+    Inner blocks are changed to air, dropping their contents as entities but not themselves. 
+    If the fill region has no inner blocks (because it is smaller than three blocks 
+    in at least one dimension), acts like replace.
+
+    KEEP: Replaces only air blocks in the fill region with the specified block.
+
+    OUTLINE: Replaces only blocks on the outer edge of the fill region with the specified block. 
+    Inner blocks are not affected. If the fill region has no inner blocks 
+    (because it is smaller than three blocks in at least one dimension), acts like replace.
+
+    REPLACE: Replaces all blocks (including air) in the fill region with the specified block,
+    without dropping blocks or block contents as entities. Optionally, instead of specifying 
+    a data tag for the replacing block, block ID and data values may be specified to limit 
+    which blocks are replaced (see replaceTileName and replaceDataValue below)
+    """
+    def __init__(self, option: str = 'replace'):
         self.option = option
 
-    def __repr__(self):
-        if self.mode == 'replace':
-            if type(self.option) is not Block:
-                raise BlockHandlerWrongType(f'The BlockHandler option provided: \'{self.option}\' is not a valid option for the block handler "replace".')
-                
-        if self.mode not in options:
-            raise BlockHandlerWrongType(f'The BlockHandler provided: \'{self.mode}\' is not a valid option. Availables: [{" | ".join(options)}]')
-        elif self.option:
-            return(f'{self.mode} {repr(self.option)}')
+
+    def __repr__(self):       
+        if self.option not in options:
+            raise BlockHandlerWrongType(f'The BlockHandler provided: \'{self.option}\' is not a valid option. Availables: [{" | ".join(options)}]')
         else:
-            return(f'{self.mode}')
+            return(f'{self.option}')
 
 class BlockHandlerWrongType(Exception):
     pass
 
 class BlockHandlerWrongOption(Exception):
     pass
-
-"""DESTROY: Replaces all blocks (including air) in the fill region with the specified block, 
-dropping the existing blocks (including those that are unchanged) and block contents 
-as entities as if they had been mined with an unenchanted diamond shovel or pickaxe. 
-(Blocks that can only be mined with shears, such as vines, will not drop; neither will liquids.)
-
-HOLLOW: Replaces only blocks on the outer edge of the fill region with the specified block. 
-Inner blocks are changed to air, dropping their contents as entities but not themselves. 
-If the fill region has no inner blocks (because it is smaller than three blocks 
-in at least one dimension), acts like replace.
-
-KEEP: Replaces only air blocks in the fill region with the specified block.
-
-OUTLINE: Replaces only blocks on the outer edge of the fill region with the specified block. 
-Inner blocks are not affected. If the fill region has no inner blocks 
-(because it is smaller than three blocks in at least one dimension), acts like replace.
-
-REPLACE: Replaces all blocks (including air) in the fill region with the specified block,
-without dropping blocks or block contents as entities. Optionally, instead of specifying 
-a data tag for the replacing block, block ID and data values may be specified to limit 
-which blocks are replaced (see replaceTileName and replaceDataValue below)"""
--- mc_api/components/test/test_BlockHandler.py
+++ mc_api/components/test/test_BlockHandler.py
@@ -6,36 +6,53 @@ from mc_api.components.Block import Block
 class TestBlockHandler(unittest.TestCase):
 
     def test_handler_destroy(self):
-        diff = BlockHandler('destroy').to_str()
+        bh = BlockHandler('destroy')
+
+        diff = repr(bh)
         test = "destroy"
         self.assertEqual(diff, test)
     
     def test_handler_replace(self):
-        block = Block('bedrock')
-        diff = BlockHandler('replace', block).to_str()
-        test = "replace minecraft:bedrock"
+        bh = BlockHandler('replace')
+
+        diff = repr(bh)
+        test = "replace"
         self.assertEqual(diff, test)
     
     def test_handler_keep(self):
-        diff = BlockHandler('keep').to_str()
+        bh = BlockHandler('keep')
+
+        diff = repr(bh)
         test = "keep"
         self.assertEqual(diff, test)
     
     def test_handler_hollow(self):
-        diff = BlockHandler('hollow').to_str()
+        bh = BlockHandler('hollow')
+
+        diff = repr(bh)
         test = "hollow"
         self.assertEqual(diff, test)
 
     def test_handler_outline(self):
-        diff = BlockHandler('outline').to_str()
+        bh = BlockHandler('outline')
+
+        diff = repr(bh)
         test = "outline"
         self.assertEqual(diff, test)
     
+    def test_handler_default(self):
+        bh = BlockHandler()
+
+        diff = repr(bh)
+        test = "replace"
+        self.assertEqual(diff, test)
+
     # This tests that BlockHandler correctly throws an exception on wrong BlockHandler type
     def test_abritrary_handler(self):
         passed = False
         try:
-            BlockHandler('imaginary').to_str()
+            bh = BlockHandler('imaginary')
+            repr(bh)
             passed = False
         except BlockHandlerWrongType:
             passed = True
