c402b635b98b41b81ce3ee4215f994a61dbba8cc
PortalHubYT <portalhub.skyblock@gmail.com>
2021-05-25 13:11:09 +0000

    updated the "composed" test_suite to be relevant with the recent changes

--- mc_api/components/test/test_composed.py
+++ mc_api/components/test/test_composed.py
@@ -9,17 +9,20 @@ from mc_api.components.BlockState import BlockState
 
 class TestBlockHandler(unittest.TestCase):
     def test_handler_real_case(self):
-        diff = Command('setblock', Coordinates(0, 0, 0), Block('bedrock'), BlockHandler('destroy')).to_str()
-        test = "setblock 0 0 0 minecraft:bedrock destroy"
+        command = Command('setblock', Coordinates(0, 0, 0), Block('bedrock'), BlockHandler('destroy'))
+
+        diff = repr(command)
+        test = "setblock 0 0 0 minecraft:bedrock[]{} destroy"
         self.assertEqual(diff, test)
 
-    def test_fill_wblockstate(self):
+    def test_fill_with_blockstate(self):
         pos1 = Coordinates(0, 4, 0)
         pos2 = Coordinates(10, 4, 10)
-        facing = BlockState("facing", "north")
-        half = BlockState("half", "top")
-        block = Block("oak_stairs", blockstate = [facing, half])
+        bs = BlockState({"facing": "north", "half": "top"})
+        block = Block("oak_stairs", blockstate = bs)
 
         command = Command('fill', pos1, pos2, block)
-        expected = "fill 0 4 0 10 4 10 minecraft:oak_stairs[facing=north,half=top]"
-        self.assertEqual(command.to_str(), expected)
+
+        diff = repr(command)
+        expected = "fill 0 4 0 10 4 10 minecraft:oak_stairs[facing=north,half=top]{}"
+        self.assertEqual(diff, expected)
