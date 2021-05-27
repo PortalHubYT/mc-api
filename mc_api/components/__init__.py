f60079a74447d41c4c1591a942381e118b8266b7
MrCyclopede <MrCyclopede@github.com>
2021-05-27 17:37:17 +0000

    removed Command

--- mc_api/components/__init__.py
+++ mc_api/components/__init__.py
@@ -2,7 +2,6 @@ from .Block import Block
 from .BlockCoordinates import BlockCoordinates
 from .BlockHandler import BlockHandler
 from .BlockState import BlockState
-from .Command import Command
 from .Coordinates import Coordinates
 from .Entity import Entity
 from .Message import Message
@@ -11,11 +10,19 @@ from .TargetSelector import TargetSelector
 
 from .Zone import Zone
 
-components = ['Block', 'BlockHandler', 'BlockCoordinates', 'BlockState', 
-                'Command', 'Coordinates', 'Entity', 'Message', 
-                'NBT', 'TargetSelector']
+components = [
+    "Block",
+    "BlockHandler",
+    "BlockCoordinates",
+    "BlockState",
+    "Coordinates",
+    "Entity",
+    "Message",
+    "NBT",
+    "TargetSelector",
+]
 
-custom_components = ['Zone']
+custom_components = ["Zone"]
 
 functions = []
 
