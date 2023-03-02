881766c342f931ce26f56f3a87afa9a6db10409d
atyrode <alex.tyrode@outlook.fr>
2023-03-02 21:16:04 +0100

    forgot to upgrade version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='shulker',
-    version='0.3.2',
+    version='0.3.3',
     author='PortalHubYT',
     author_email='portalhub.business@gmail.com',
     description='A minecraft interface using RCON',
