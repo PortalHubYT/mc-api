5bfeccbbfaddea67554d7c553972a80ff47053af
atyrode <alex.tyrode@outlook.fr>
2023-03-02 18:53:38 +0100

    making a test with the module init

--- shulker/__init__.py
+++ shulker/__init__.py
@@ -1,3 +1,5 @@
+print("Initializing shulker")
+
 # Classes
 from .server.connect import RconClient as connect
 from .server.create import DockerInstance as create
@@ -5,7 +7,6 @@ from .components import *
 from .functions import *
 
 # Functions
-
 from .functions.set_text import set_text, _set_text
 from .functions.set_zone import set_zone, _set_zone
 from .functions.set_block import set_block, _set_block
