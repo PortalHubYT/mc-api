9fe8d5145868fe1faceb01eddb9b73ea9108142b
atyrode <alex.tyrode@outlook.fr>
2023-03-07 13:59:36 +0100

    forgot to bump version and remove get_block from init

--- shulker/functions/__init__.py
+++ shulker/functions/__init__.py
@@ -1,5 +1,4 @@
 from .base_functions import nest_commands, entity_list
-from .get_block import get_block, meta_get_block
 from .set_block import set_block, meta_set_block
 from .set_image import set_image, meta_set_image, print_palette
 from .set_text import set_text, meta_set_text
