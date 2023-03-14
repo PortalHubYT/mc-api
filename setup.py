06b93e3a20f20529d6a802c8b7b174ae78943cea
atyrode <alex.tyrode@outlook.fr>
2023-03-14 19:56:56 +0100

    update to add missing .json depedencies

--- setup.py
+++ setup.py
@@ -6,14 +6,14 @@ with open('requirements.txt') as f:
 
 setup(
     name='shulker',
-    version='0.4.5',
+    version='0.4.6',
     author='PortalHubYT',
     author_email='portalhub.business@gmail.com',
     description='A minecraft interface using RCON',
     url='https://github.com/PortalHubYT/mc_api',
     packages=find_packages(),
     package_data={
-        'shulker': ['functions/*.json', 'components/*.json', 'server/*.json']
+        'shulker': ['functions/*.json', 'components/*.json', 'server/*.json', 'functions/mc_data/*.json']
     },
     # install requires should pull the list from requirements.txt
     install_requires=requirements
