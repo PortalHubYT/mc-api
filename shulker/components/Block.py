f575baf01c5e2a715bd34f4ab983098f481492dc
atyrode <alex.tyrode@outlook.fr>
2023-03-22 22:58:46 +0100

    Block got the #8 treatment as well as full revamp including .set() .move() and .offset()

--- shulker/components/Block.py
+++ shulker/components/Block.py
@@ -2,8 +2,25 @@ import os
 import json
 
 from .BlockState import BlockState
+from .BlockCoordinates import BlockCoordinates
+from .Coordinates import Coordinates
 from .NBT import NBT
-    
+
+path = os.path.dirname(os.path.abspath(__file__))
+with open(f'{path}/../metadata/data/block_list.json', 'r') as file:
+    blocks_data = json.load(file)
+
+blocks = {}
+for key in blocks_data:
+    if 'properties' in blocks_data[key]:
+        blocks[key] = {}
+        blocks[key]['properties'] = blocks_data[key]['properties']
+        for states in blocks_data[key]['states']:
+            if 'default' in states.keys():
+                blocks[key]['default'] = states['properties']
+    else:
+        blocks[key] = {'properties': {}, 'default': {}}
+
 class Block:
     """
     A single '<block>' argument looks like this:
@@ -46,16 +63,101 @@ class Block:
 
         if ":" in id:
             parsed = id.split(":")
-            self.namespace = parsed[0]
-            self.id = parsed[1]
+            self.__dict__['namespace'] = parsed[0]
+            self.__dict__['id'] = parsed[1]
         else:
-            self.namespace = "minecraft"
-            self.id = id
+            self.__dict__['namespace'] = "minecraft"
+            self.__dict__['id'] = id.replace(" ", "_")
+            
+        self.__dict__['descriptor'] = self.__dict__['namespace'] + ":" + self.__dict__['id']
+            
+        if self.__dict__['namespace'] == "minecraft":
+            if self.__dict__['descriptor'] not in blocks:
+                raise ValueError(f"Unknown block ID: {id}")
 
-        self.blockstate = (
-            blockstate if isinstance(blockstate, BlockState) else BlockState()
-        )
-        self.nbt = nbt if isinstance(nbt, NBT) else NBT(nbt)
+        self.__dict__['coords'] = None
+        
+        if self.__dict__['namespace'] == "minecraft":
+            self.__dict__['blockstate'] = (
+                blockstate if isinstance(blockstate, BlockState) else BlockState(blocks[self.descriptor]['default'])
+            )
+        else:
+            self.__dict__['blockstate'] = (
+                blockstate if isinstance(blockstate, BlockState) else BlockState({})
+            )
+        self.__dict__['nbt'] = nbt if isinstance(nbt, NBT) else NBT(nbt)
 
+    @property
+    def attributes(self):
+        return blocks[self.descriptor]['properties']
+    
+    def set(self, *args, handler=None):
+        from shulker.functions.default import set_block
+        if len(args) == 3:
+            coords = (args[0], args[1], args[2])
+        elif len(args) == 1:
+            coords = args[0]
+        else:
+            raise ValueError(f"set takes 1 (tuple, list, Coordinates...) or 3 (x, y, z) arguments and eventually an handler=.., not {len(args)}")
+        
+        self.__dict__['coords'] = coords
+        
+        if handler:
+            return set_block(coords, self, handler)
+        else:
+            return set_block(coords, self)
+    
+    def move(self, *args, handler=None, replace_by="air"):
+        from shulker.functions.default import set_block
+        if len(args) == 3:
+            new_coords = (args[0], args[1], args[2])
+        elif len(args) == 1:
+            new_coords = args[0]
+        else:
+            raise ValueError(f"move takes 1 (tuple, list, Coordinates...) or 3 (x, y, z) arguments and eventually an handler=.. and replace_by=.., not {len(args)}")
+        
+        old_coords = self.__dict__['coords']
+        self.__dict__['coords'] = new_coords
+        
+        if handler:
+            set_block(old_coords, replace_by, handler)
+            set_block(new_coords, self, handler)
+        else:
+            set_block(old_coords, replace_by)
+            set_block(new_coords, self)
+    
+    def offset(self, x=0, y=0, z=0, handler=None, replace_by="air"):
+
+        if type(self.__dict__['coords']) in [tuple, list]:
+            to_offset = BlockCoordinates(*self.__dict__['coords'])
+        elif type(self.__dict__['coords']) in [BlockCoordinates, Coordinates]:
+            to_offset = self.__dict__['coords']
+            
+        new_coords = to_offset.offset(x=x, y=y, z=z)
+        
+        self.move(new_coords, handler=handler, replace_by=replace_by)
+        
+    def __setattr__(self, name, value):
+        if name == "nbt":
+            if type(value) == NBT:
+                self.__dict__['nbt'] = value
+            else:
+                self.__dict__['nbt'] = NBT(value)
+        elif name == "blockstate":
+            if type(value) == BlockState:
+                self.__dict__['blockstate'] = value
+            else:
+                self.__dict__['blockstate'] = BlockState(value)
+        elif name in self.attributes and value in self.attributes[name]:
+            setattr(self.blockstate, name, value)
+        else:
+            raise ValueError(f"BlockState '{name}={value}' for '{self.descriptor}' does not exist")
+    
+    def __getattr__(self, name):
+        if name in self.attributes:
+            return getattr(self.blockstate, name)
+        else:
+            return getattr(self, name)
+    
     def __str__(self):
-        return f"{self.namespace}:{self.id}{self.blockstate}{self.nbt}"
+        return f"{self.descriptor}{self.blockstate}{self.nbt}"
--- shulker/components/test/test_Block.py
+++ shulker/components/test/test_Block.py
@@ -6,29 +6,16 @@ from shulker.components.NBT import NBT
 
 
 class TestBlock(unittest.TestCase):
-    def test_block_bedrock(self):
-        block = Block("bedrock")
-
-        diff = str(block)
-        test = "minecraft:bedrock[]{}"
-        self.assertEqual(diff, test)
-
     def test_block_namespace(self):
+        """Test constructor with a valid non-minecraft namespace"""
         block = Block("mod:op_block")
 
         diff = str(block)
         test = "mod:op_block[]{}"
         self.assertEqual(diff, test)
 
-    def test_block_simple_blockstate(self):
-        block = Block("bedrock")
-        block.blockstate = BlockState({"facing": "north"})
-
-        diff = str(block)
-        test = "minecraft:bedrock[facing=north]{}"
-        self.assertEqual(diff, test)
-
     def test_block_double_blockstate(self):
+        """Test constructor with a valid blockstate of 2 elements"""
         block = Block("bedrock")
         block.blockstate = BlockState({"facing": "north", "half": "top"})
 
@@ -36,23 +23,8 @@ class TestBlock(unittest.TestCase):
         test = "minecraft:bedrock[facing=north,half=top]{}"
         self.assertEqual(diff, test)
 
-    def test_block_simple_nbt(self):
-        block = Block("bedrock")
-        block.nbt = NBT({"Fire": 4})
-
-        diff = str(block)
-        test = "minecraft:bedrock[]{Fire:4}"
-        self.assertEqual(diff, test)
-
-    def test_block_double_nbt(self):
-        block = Block("bedrock")
-        block.nbt = NBT({"Fire": 4, "Air": 0})
-
-        diff = str(block)
-        test = "minecraft:bedrock[]{Fire:4,Air:0}"
-        self.assertEqual(diff, test)
-
     def test_block_mixed(self):
+        """Test constructor with a valid blockstate, and a valid NBT"""
         block = Block("bedrock")
         block.nbt = NBT({"Fire": 4, "Air": 0})
         block.blockstate = BlockState({"facing": "north", "half": "top"})
@@ -60,11 +32,59 @@ class TestBlock(unittest.TestCase):
         diff = str(block)
         test = "minecraft:bedrock[facing=north,half=top]{Fire:4,Air:0}"
         self.assertEqual(diff, test)
+            
+    def test_constructor_valid_block_id(self):
+        """Test constructor with a valid block ID."""
+        block = Block("minecraft:stone")
+        self.assertEqual(block.descriptor, "minecraft:stone")
 
-    def test_empty(self):
+    def test_constructor_invalid_block_id(self):
+        """Test constructor with an invalid block ID."""
+        with self.assertRaises(ValueError):
+            Block("minecraft:non_existent_block")
+            
         with self.assertRaises(ValueError):
             Block("")
 
+    def test_constructor_default_namespace(self):
+        """Test constructor with default namespace (no namespace provided)."""
+        block = Block("stone")
+        self.assertEqual(block.descriptor, "minecraft:stone")
+
+    def test_constructor_blockstate(self):
+        """Test constructor with a valid block state."""
+        blockstate = BlockState({"facing": "north"})
+        block = Block("minecraft:furnace", blockstate=blockstate)
+        self.assertEqual(block.blockstate, blockstate)
+
+    def test_constructor_nbt(self):
+        """Test constructor with a valid NBT."""
+        nbt = NBT({"BurnTime": 200})
+        block = Block("minecraft:furnace", nbt=nbt)
+        self.assertEqual(block.nbt, nbt)
+
+    def test_set_blockstate(self):
+        """Test setting block state."""
+        block = Block("minecraft:furnace")
+        block.facing = "north"
+        self.assertEqual(block.facing, "north")
+
+    def test_get_blockstate(self):
+        """Test getting block state."""
+        block = Block("minecraft:furnace")
+        block.facing = "north"
+        self.assertEqual(block.facing, "north")
+
+    def test_invalid_blockstate(self):
+        """Test setting an invalid block state."""
+        block = Block("minecraft:furnace")
+        with self.assertRaises(ValueError):
+            block.invalid_state = "value"
+
+    def test_str_representation(self):
+        """Test string representation of a block."""
+        with self.assertRaises(ValueError):
+            Block("minecraft:furnace[facing=north]{BurnTime:200}")
 
 if __name__ == "__main__":
     unittest.main()
